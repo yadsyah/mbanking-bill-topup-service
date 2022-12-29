9ca21b31a4e3199810c655dd7fab454b81930681
yadsyah <diyansetiyadi@gmail.com>
2022-12-29 17:48:05 +0700

    init commit

---
+++ src/main/java/id/co/diansetiyadi/billtopupservice/BillTopupServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.billtopupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillTopupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillTopupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/diansetiyadi/billtopupservice/BillTopupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.billtopupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillTopupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
